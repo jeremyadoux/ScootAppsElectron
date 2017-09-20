7ac4ca09577491409294185fe52a3b1331926cdc
jadoux <jeremy.adoux@lynkoa.com>
2017-09-20 16:41:37 +0200

    Rajout d'un mode spécifique pour le test de la recette par des utilisateurs lynkoa

--- src/app.js
+++ src/app.js
@@ -1,17 +1,25 @@
 // Here is the starting point for your application code.
-// All stuff below is just to show you how it works. You can delete all of it.
 
-// Use new ES6 modules syntax for everything.
-import os from 'os'; // native node.js module
-import { remote } from 'electron'; // native electron module
-import jetpack from 'fs-jetpack'; // module loaded from npm
+// All stuff below is just to show you how it works. You can delete all of it.
+import { remote } from 'electron';
+import jetpack from 'fs-jetpack';
 import env from './env';
 
-var app = remote.app;
-var appDir = jetpack.cwd(app.getAppPath());
+const app = remote.app;
+const appDir = jetpack.cwd(app.getAppPath());
 
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('greet').innerHTML = greet();
-    document.getElementById('platform-info').innerHTML = os.platform();
-    document.getElementById('env-name').innerHTML = env.name;
-});
+// Holy crap! This is browser window with HTML and stuff, but I can read
+// here files form disk like it's node.js! Welcome to Electron world :)
+const manifest = appDir.read('package.json', 'json');
+
+const osMap = {
+    win32: 'Windows',
+    darwin: 'macOS',
+    linux: 'Linux',
+};
+
+document.querySelector('#greet').innerHTML = greet();
+document.querySelector('#os').innerHTML = osMap[process.platform];
+document.querySelector('#author').innerHTML = manifest.author;
+document.querySelector('#env').innerHTML = env.name;
+document.querySelector('#electron-version').innerHTML = process.versions.electron;
--- src/background.js
+++ src/background.js
@@ -1,69 +1,47 @@
-(function () {'use strict';
 
-    function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+import { app, ipcMain, BrowserWindow } from 'electron';
 
-    var electron = require('electron');
-    var promise = _interopDefault(require('promise'));
-    var http = _interopDefault(require('http'));
-    var storage = _interopDefault(require('electron-json-storage'));
-    var jetpack = _interopDefault(require('fs-jetpack'));
-    var Toaster = _interopDefault(require('electron-toaster'));
+import env from './env';
 
-// Simple wrapper exposing environment variables to rest of the code.
-
-// The variables have been written to `env.json` by the build process.
-    var env = jetpack.cwd(__dirname).read('env.json', 'json');
-
-    const xml2js = require('xml2js');
-
-    let toaster = new Toaster();
-    var winCreateTicket = null;
+let storage = require('electron-json-storage');
+let winCreateTicket = null;
 
 
-    electron.app.on('ready', () => {
-        openWindowsCreateTicket();
+app.on('ready', () => {
+    openWindowsCreateTicket();
+});
+
+
+ipcMain.on('redmine-save-favorite', (event, arg) => {
+    storage.set(env.redmineFavorite, arg, (error) => {
+        if (error) throw error;
     });
+});
 
-    electron.app.on('will-quit', () => {
-        // Unregister all shortcuts.
-        electron.globalShortcut.unregisterAll();
+ipcMain.on('redmine-get-favorite', (event, arg) => {
+    storage.get(env.redmineFavorite, (error, redmineFavorite) => {
+        event.sender.send('redmine-return-favorite', redmineFavorite);
     });
+});
 
-
-    electron.ipcMain.on('redmine-save-favorite', (event, arg) => {
-        storage.set(env.redmineFavorite, arg, (error) => {
-            if (error) throw error;
-        });
-    });
-
-    electron.ipcMain.on('redmine-get-favorite', (event, arg) => {
-        storage.get(env.redmineFavorite, (error, redmineFavorite) => {
-            event.sender.send('redmine-return-favorite', redmineFavorite);
-        });
-    });
-
-    electron.ipcMain.on('reload-windows', (event, arg) => {
-        if(winCreateTicket != null) {
-            winCreateTicket.show();
-            winCreateTicket.loadURL(`file://${__dirname}/templates/ticket-index.html`);
-        }
-    });
-
-    function openWindowsCreateTicket() {
-        if(winCreateTicket != null) {
-            winCreateTicket.show();
-            winCreateTicket.loadURL(`file://${__dirname}/templates/ticket-index.html`);
-        } else {
-            // Create the browser window.
-            winCreateTicket = new electron.BrowserWindow({width: 1680, height: 960, icon:  __dirname + '/images/scout.ico'});
-
-            toaster.init(winCreateTicket);
-
-            // and load the index.html of the app.
-            winCreateTicket.openDevTools();
-            winCreateTicket.loadURL(`file://${__dirname}/templates/ticket-index.html`);
-            winCreateTicket.maximize();
-        }
+ipcMain.on('reload-windows', (event, arg) => {
+    if(winCreateTicket != null) {
+        winCreateTicket.show();
+        winCreateTicket.loadURL(`file://${__dirname}/templates/ticket-index.html`);
     }
-}());
-//# sourceMappingURL=background.js.map
+});
+
+function openWindowsCreateTicket() {
+    if(winCreateTicket != null) {
+        winCreateTicket.show();
+        winCreateTicket.loadURL(`file://${__dirname}/templates/ticket-index.html`);
+    } else {
+        // Create the browser window.
+        winCreateTicket = new BrowserWindow({width: 1680, height: 960, icon:  __dirname + '/images/scout.ico'});
+
+        // and load the index.html of the app.
+        winCreateTicket.openDevTools();
+        winCreateTicket.loadURL(`file://${__dirname}/templates/ticket-index.html`);
+        winCreateTicket.maximize();
+    }
+}
